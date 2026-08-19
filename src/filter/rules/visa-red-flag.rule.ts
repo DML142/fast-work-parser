2a266406e3b8801937dfacfc04201e1ce0e990b9
Demolov <demolovfennec@gmail.com>
2026-08-19 09:23:22 +0300

    feat: add VisaRedFlagRule

---
+++ src/filter/rules/visa-red-flag.rule.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { FilterRule } from '../../common/interfaces/filter-rule.interface';
+import { JobEntity } from '../../jobs/entities/job.entity';
+
+export const DEFAULT_VISA_RED_FLAGS: readonly string[] = [
+  'US citizen',
+  'must be authorized to work in the US without sponsorship',
+  'security clearance',
+  'no visa sponsorship',
+  'must be based in the US',
+  'must be based in the UK',
+];
+
+@Injectable()
+export class VisaRedFlagRule implements FilterRule {
+  readonly name = 'VisaRedFlagRule';
+
+  constructor(private readonly redFlags: readonly string[]) {}
+
+  matches(job: JobEntity): boolean {
+    const haystack = job.description.toLowerCase();
+    return !this.redFlags.some((flag) => haystack.includes(flag.toLowerCase()));
+  }
+}
